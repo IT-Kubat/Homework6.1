92abd4782ba0b87e399a35464eadb7ee05b15e13
IT-kubat <kubat88@list.ru>
2019-11-11 21:44:29 +0600

    Initial commit

---
+++ src/com/company/Warrior.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Warrior extends Player {
+    private int hitType;
+
+    public Warrior(int health, String damage, int hitType) {
+        super(health, damage);
+        this.hitType = hitType;
+    }
+
+    public int getHitType() {
+        return hitType;
+    }
+
+    public void setHitType(int hitType) {
+        this.hitType = hitType;
+    }
+}
